89343c2d5f26fe68af0ac750d2e13891f980fcab
Evgeny <zhen-kok@yandex.ru>
2016-09-22 17:04:24 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -7,11 +7,3 @@ return array(
   'user' => 'root',
   'password' => ''
 );
-/*
-return array(
-    'host' => 'localhost',
-    'dbname' => 'anything_snus',
-    'user' => 'anything_snus',
-    'password' => 'unclesnus240816'
-);
-*/
